d049b70969ca593df0cf1d372c5b010ec4ad9e1a
koliadervyanko <kyderevyanko@gmail.com>
2022-10-23 16:08:43 +0300

    Add button

---
+++ script.js
@@ -0,0 +1,6 @@
+function link(){
+    let ans = confirm('Вы доверяете ссылке по которой переейдете?');
+    if (ans == true) {
+        window.location.href = 'https://www.youtube.com/watch?v=HR5yikLQHn0'
+    }
+}
