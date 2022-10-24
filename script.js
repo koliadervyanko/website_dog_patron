bb1afce649d5dd6a24cc3192dc2913be28a45357
koliadervyanko <kyderevyanko@gmail.com>
2022-10-24 09:57:15 +0300

    Edit text when your klickthe button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function link(){
-    let ans = confirm('Вы доверяете ссылке по которой переейдете?');
+    let ans = confirm('Вы уверены что хотите перейти по ссылке?');
     if (ans == true) {
         window.location.href = 'https://www.youtube.com/watch?v=HR5yikLQHn0'
     }
